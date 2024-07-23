afd16e37c85aab8c94dbb095ef296c6807dbf0b1
Andre Pugliesi <andre.cezar.silva@gmail.com>
2024-07-23 11:04:16 -0300

    Add files via upload

---
+++ Scripts_bash_auxiliares/zz-loop_simulacoes_libradtran_trans4_4-lwcsemcal.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Iterando por todos os itens de um diretório
+
+#pasta=/home/andre/libRadtran-2.0.4/codigos_doutorado/
+pasta=/home/andre/libRadtran-2.0.4/copias_inps_trans4-final/
+
+echo $pasta
+#ls $pasta
+cd $pasta
+#pwd
+
+arquivos=$(ls $pasta_limpos)
+for arquivo in $arquivos
+do
+	if [ -f "$arquivo" ]
+	then
+		echo Executando $arquivo e gerando ${arquivo/.inp/.out}........
+		../bin/uvspec < $arquivo > ${arquivo/.inp/.out} 
+		#(../bin/uvspec < $arquivo > ${arquivo/.inp/.out}) >& ${arquivo/.inp/_verbose.txt}
+	fi
+        #echo Executando $arquivo e gerando ${arquivo/.inp/.out}........
+        #(..../bin/uvspec < $arquivo > ${arquivo/.inp/.out}) >& ${arquivo/.inp/_verbose.txt}
+done
